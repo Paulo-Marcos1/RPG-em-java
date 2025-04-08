a7e07fbd3c81928e56a3ddae57b171df8897d6f3
Paulo Marcos <132699727+Paulo-Marcos1@users.noreply.github.com>
2025-04-08 15:51:51 -0300

    Update Main.java

--- RPG do senhor Sabio/Main.java
+++ RPG do senhor Sabio/Main.java
@@ -7,22 +7,56 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        while(true) {
+        Personagem jogador = null;
+        Monstro monstro = new Monstro();
+
+        while(jogador == null) {
             System.out.print("Digite 1: Para mago \nDigite 2: Para arqueiro \nDigite 3 Para guerreiro \nEscolha:");
             int escolha = input.nextInt();
+            input.nextLine();
             switch (escolha) {
                 case 1:
-                    new Mago();
-                    System.out.println("mago");
+                    jogador = new Mago();
+                    System.out.println("Você escolheu ser um Mago!");
                     break;
                 case 2:
-                    new Arqueiro();
-                    System.out.println("arqueiro");
+                    jogador = new Arqueiro();
+                    System.out.println("Você escolheu ser um arqueiro!");
                     break;
                 case 3:
-                    new Guerreiro();
-                    System.out.println("Guerreiro");
+                    jogador = new Guerreiro();
+                    System.out.println("Você escolheu ser um Guerreiro!");
+                    break;
+
+                default:
+                    System.out.println("opção invalida!");
+
             }
         }
+        System.out.print("Digite seu nome: ");
+        String nome = input.nextLine();
+        jogador.setNome(nome);
+        System.out.println("Olá "+ jogador.getClasse() + " "+jogador.getNome() + " começará agora sua nova aventura!");
+        System.out.println(monstro.getNome());
+        int turno = 0;
+        while(true) {
+
+            System.out.println("Turno "+ turno++ +" ataque do " + jogador.getNome());
+            jogador.atacar();
+            monstro.receberataque(jogador.ataque());
+
+            System.out.println("Turno "+ turno++ +" ataque do " + monstro.getNome());
+            monstro.atacar();
+            jogador.receberataque(monstro.ataque());
+
+
+
+
+             break;
+
+
+        }
+
     }
 }
+
